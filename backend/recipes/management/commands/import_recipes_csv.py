265d225a552c6fcd0a705da3b8fc2254cf3cbab8
HikkiAdvent <valerpetrencko@yandex.ru>
2025-01-17 00:35:35 +0300

    изменена логика добавления рецептов через команду

--- backend/recipes/management/commands/import_recipes_csv.py
+++ backend/recipes/management/commands/import_recipes_csv.py
@@ -1,8 +1,9 @@
+import os
 import csv
 
-from django.contrib.auth import get_user_model
+from django.core.files import File
 from django.core.management.base import BaseCommand
-
+from django.contrib.auth import get_user_model
 from recipes.models import Recipe, Ingredient, Tag
 
 User = get_user_model()
@@ -29,27 +30,33 @@ class Command(BaseCommand):
                 ingredients = Ingredient.objects.filter(
                     id__in=row['ingredients'].split(';')
                 )
-                image_path = row['image']
-                recipe, created = Recipe.objects.get_or_create(
-                    author=author,
-                    name=row['name'],
-                    text=row['text'],
-                    cooking_time=row['cooking_time'],
-                    image=image_path
-                )
-                if created:
-                    for ingredient in ingredients:
-                        recipe.ingredients.add(ingredient)
-                    for tag in tags:
-                        recipe.tags.add(tag)
-                    self.stdout.write(
-                        self.style.SUCCESS(
-                            f'Рецепт "{recipe.name}" успешно добавлен.'
-                        )
-                    )
-                else:
+                image_path = os.path.join('foodgram_backend/data/', row['image'])
+                recipes = Recipe.objects.filter(author=author, name=row['name'])
+                if recipes.exists():
+                    recipe = recipes.first()
                     self.stdout.write(
                         self.style.WARNING(
                             f'Рецепт "{recipe.name}" уже существует.'
+                        )
+                    )
+                else:
+                    recipe = Recipe(
+                        author=author,
+                        name=row['name'],
+                        text=row['text'],
+                        cooking_time=row['cooking_time']
+                    )
+                    with open(image_path, 'rb') as image_file:
+                        recipe.image.save(
+                            os.path.basename(image_path),
+                            File(image_file),
+                            save=False
+                        )
+                    recipe.save()
+                    recipe.ingredients.add(*ingredients)
+                    recipe.tags.add(*tags)
+                    self.stdout.write(
+                        self.style.SUCCESS(
+                            f'Рецепт "{recipe.name}" успешно добавлен.'
                         )
                     )
